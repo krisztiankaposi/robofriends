c469e9ac2a99422852f0238121ff11e6bbf666ab
krisztian <krisztiankaposi@hotmail.com>
2022-11-08 12:03:14 +0000

    new commit

---
+++ src/card.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+const card =()=>{
+
+    return(
+<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+<img alt='robots' src='https://robohash.org/test?200x200'/>
+<div>
+    <h2>jane doe</h2>
+    <p>jane.doe@gmail.com</p>
+</div>
+
+</div>
+
+    );
+}
+export default card;
