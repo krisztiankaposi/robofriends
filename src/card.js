d7e6baa9af84f61ba673a4156f9d26c084cfe41e
krisztian kaposi <krisztiankaposi@hotmail.com>
2022-11-08 13:17:53 +0000

    new pic cards

--- src/card.js
+++ src/card.js
@@ -1,13 +1,14 @@
 import react from 'react';
 
-const card =()=>{
+const card =({name, email, id})=>{
+    
 
     return(
-<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-<img alt='robots' src='https://robohash.org/test?200x200'/>
+<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+<img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
 <div>
-    <h2>jane lickyyyy</h2>
-    <p>jane.doe@gmail.com</p>
+    <h2>{name}</h2>
+    <p>{email}</p>
 </div>
 
 </div>
